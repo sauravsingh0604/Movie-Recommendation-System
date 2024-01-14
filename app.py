fdf20bc8b00bd85ed7ae68a37e927f43d2999f7d
unknown <saurav0604singh@gmail.com>
2024-01-14 23:07:27 +0530

    Add .gitattributes to handle line endings

---
+++ app.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import requests
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_poster = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        # Fetch poster
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_poster
+
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox('Select a movie:', movies['title'].values)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+    
+    # Display recommendations in columns
+    cols = st.columns(5)
+    
+    for i in range(5):
+        cols[i].image(posters[i], caption=names[i], use_column_width=True)
